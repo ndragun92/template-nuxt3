8f94d157b46c662cd0f84425061e328e24f6dd85
ndragun <nemanjadragun92@gmail.com>
2025-06-11 13:28:37 +0200

    Update dependencies in `package-lock.json` – various packages bumped to latest versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
     },
     asyncContext: true,
     appManifest: false,
-    buildCache: true, // Build Cache
+    buildCache: false, // Build Cache
   },
 
   imports: {
