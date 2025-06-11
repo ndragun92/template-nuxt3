294f3a4c30255469592bd8b10f5a2892349753f7
ndragun <nemanjadragun92@gmail.com>
2025-06-11 13:36:32 +0200

    fix: resolve TypeScript property access issue and add placeholder JSON file in `.well-known` directory

--- app/plugins/init.ts
+++ app/plugins/init.ts
@@ -2,7 +2,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   let host: string;
   // Get host in case of server side request
   if (import.meta.env.SSR) {
-    host = nuxtApp?.ssrContext?.event?.req?.headers?.host || "";
+    host = nuxtApp?.ssrContext?.event?.node?.req?.headers?.host || "";
   } else {
     host = window.location.host;
   }
